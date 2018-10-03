1b3ad288af96b70e6a2ccead3e7db8db471a6445
zhijial <624636394@qq.com>
2018-10-03 17:14:49 +1000

    123321

--- pacman-contest/myTeam.py
+++ pacman-contest/myTeam.py
@@ -22,7 +22,7 @@ import game
 #################
 
 def createTeam(firstIndex, secondIndex, isRed,
-               first = 'DummyAgent', second = 'DummyAgent'):
+               first = 'Agent', second = 'DummyAgent'):
   """
   This function should return a list of two agents that will form the
   team, initialized using firstIndex and secondIndex as their agent
